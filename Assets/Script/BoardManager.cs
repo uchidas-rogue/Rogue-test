6699d499e76a2421dadb7eadd939e861cd567849
uchidas <sho417sho@gmail.com>
2020-05-18 23:55:37 +0900

    digmaze

--- Assets/Script/BoardManager.cs
+++ Assets/Script/BoardManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    public int height = 8;
-    public int width = 8;
+    public int height = 11;
+    public int width = 11;
 
     //floor prefab 
     public GameObject[] floorTiles;
@@ -69,11 +69,155 @@ public class BoardManager : MonoBehaviour
         }
     }
 
+    private enum Direction
+    {
+        none = 0,
+        up = 1,
+        down = 2,
+        left = 3,
+        right = 4
+    }
+
+    private int[,] Maze;
+
+    private void DigMaze()
+    {
+        Maze = new int[width,height];
+
+        Direction Dir = Direction.none;
+        int x = 0;
+        int y = 0;
+
+        Maze[x,y] = 1;
+
+        for (int i=0;i<121;i++)
+        {
+            while (Dir == Direction.none)
+            {
+                Dir = (Direction) Random.Range(0,4);
+            }
+            switch (Dir)
+            {
+                case Direction.up:
+                    if ((y + 2 > -1 && y + 2 < height) && (Maze[x,y+2] != 1))
+                    {
+                        y++;
+                        Maze[x,y] = 1;
+                        y++;
+                        Maze[x,y] = 1;
+                    }
+                    else
+                    {               
+                        while (true)
+                        {
+                            x = Random.Range(0,width-1);
+                            y = Random.Range(0,height-1);
+                            if (x % 2 == 0 && y % 2 == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                case Direction.down:
+                    if ((y - 2 > -1 && y - 2 < height) && (Maze[x,y-2] != 1))
+                    {
+                        y--;
+                        Maze[x,y] = 1;
+                        y--;
+                        Maze[x,y] = 1;
+                    }
+                    else
+                    {
+                        while (true)
+                        {
+                            x = Random.Range(0,width-1);
+                            y = Random.Range(0,height-1);
+                            if (x % 2 == 0 && y % 2 == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                case Direction.right:
+                    if ((x + 2 > -1 && x + 2 < width) && (Maze[x+2,y] != 1))
+                    {
+                        x++;
+                        Maze[x,y] = 1;
+                        x++;
+                        Maze[x,y] = 1;
+                    }
+                    else
+                    {
+                        while (true)
+                        {
+                            x = Random.Range(0,width-1);
+                            y = Random.Range(0,height-1);
+                            if (x % 2 == 0 && y % 2 == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                case Direction.left:
+                    if ((x - 2 > -1 && x - 2 < width) && (Maze[x-2,y] != 1))
+                    {
+                        x--;
+                        Maze[x,y] = 1;
+                        x--;
+                        Maze[x,y] = 1;
+                    }
+                    else
+                    {
+                        while (true)
+                        {
+                            x = Random.Range(0,width-1);
+                            y = Random.Range(0,height-1);
+                            if (x % 2 == 0 && y % 2 == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+            }  
+        }
+    }
+
+    private void BoardSetup2()
+    {
+        //create parent object
+        this.boardHolder = new GameObject("Board").transform;
+        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                //floor
+                //if floor at random [0] => [Random.Range(0, floorTiles.Length)]
+                if (Maze[x,y] == 1)
+                {
+                    GameObject toInstantiate = floorTiles[0];
+
+                    //Clone the Tiles
+                    GameObject instance = Instantiate(toInstantiate, 
+                                                        new Vector3(x, y, 0), 
+                                                        Quaternion.identity,
+                                                        boardHolder) as GameObject;
+                }
+                
+            }
+        }
+    }
+
     public void SetupScene()
     {
-        this.height = Random.Range(3,10);
-        this.width = Random.Range(3,10);
-        BoardSetup();
+        //this.height = Random.Range(5,10);
+        //this.width = Random.Range(5,10);
+        DigMaze();
+        BoardSetup2();
     }
 
 }
