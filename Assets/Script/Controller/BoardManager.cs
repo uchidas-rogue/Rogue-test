e05a1740feefa05fa73fa5c2ee49e0e5f1446d91
uchidas <sho417sho@gmail.com>
2020-05-19 11:12:01 +0900

    create and change folder

---
+++ Assets/Script/Controller/BoardManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public int height;
+    public int width;
+
+    //floor prefab 
+    public GameObject[] floorTiles;
+    //wall prefab
+    public GameObject[] wallTiles;
+    //Tiles parent object 
+    private Transform boardHolder;
+
+    private void BoardSetup()
+    {
+        //create parent object
+        this.boardHolder = new GameObject("Board").transform;
+
+        for (int x = -1; x < width + 1; x++)
+        {
+            for (int y = -1; y < height + 1; y++)
+            {
+                //floor
+                //if floor at random [0] => [Random.Range(0, floorTiles.Length)]
+                GameObject toInstantiate = floorTiles[0];
+                
+                if(x == -1 && y == height)
+                {//leftup
+                    toInstantiate = wallTiles[0];
+                }
+                else if (x == -1 && y == -1)
+                {//leftdown
+                    toInstantiate = wallTiles[1];
+                }
+                else if (x == width && y == -1)
+                {//rightdown
+                    toInstantiate = wallTiles[2];
+                }
+                else if (x == width && y == height)
+                {//rightup
+                    toInstantiate = wallTiles[3];
+                }
+                else if(x == -1)
+                {//left
+                    toInstantiate = wallTiles[4];
+                }
+                else if (y == -1)
+                {//down
+                    toInstantiate = wallTiles[5];
+                }
+                else if (x == width)
+                {//right
+                    toInstantiate = wallTiles[6];
+                }
+                else if (y == height)
+                {//up
+                    toInstantiate = wallTiles[7];
+                }
+                //Clone the Tiles
+                GameObject instance = Instantiate(toInstantiate, 
+                                                    new Vector3(x, y, 0), 
+                                                    Quaternion.identity,
+                                                    boardHolder
+                                                    ) as GameObject;
+            }
+        }
+    }
+
+    private void SetTiles(GameObject Tiles, int x, int y)
+    {
+        GameObject toInstantiate = Tiles;
+        //Clone the Tiles
+        GameObject instance = Instantiate(toInstantiate, 
+                                            new Vector3(x, y, 0), 
+                                            Quaternion.identity,
+                                            boardHolder) as GameObject;
+    }
+
+    private void BoardSetup2()
+    {
+        //create parent object
+        this.boardHolder = new GameObject("Board").transform;
+
+        MakeMaze makeMaze = new MakeMaze(width,height);
+        makeMaze.DigMaze();
+        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (makeMaze.Maze[x,y] == 1)
+                {
+                    //floor
+                    //if floor at random [0] => [Random.Range(0, floorTiles.Length)]
+                    SetTiles(floorTiles[0],x,y);
+                }
+                
+            }
+        }
+    }
+
+    public void SetupScene()
+    {
+        BoardSetup2();
+    }
+
+}
---
+++ Assets/Script/Model/MakeMaze.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeMaze
+{
+    public int[,] Maze;
+    private int width;
+    private int height;
+    private Direction direction;
+    private int x;
+    private int y;
+    public MakeMaze(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    private enum Direction
+    {
+        none = 0,
+        up = 1,
+        down = -1,
+        left = -2,
+        right = 2
+    }
+
+    private void ChangeDir()
+    {
+        while (true)
+        {
+            direction = (Direction) Random.Range(-2,2);
+            if (direction != Direction.none){break;}
+        }
+    }
+
+    private void ChangePosition()
+    {
+        while (true)
+        {//change position where floor tiles exist
+            x = Random.Range(0,width-1);
+            y = Random.Range(0,height-1);
+            if (x % 2 == 1 && y % 2 == 1 && Maze[x,y] == 1){break;}
+        }
+    }
+
+    public void DigMaze()
+    {
+        Maze = new int[width,height];
+        //entry position
+        x = width - 2;
+        y = height - 2;
+        //deploy floor tile
+        Maze[x,y] = 1;
+
+        for (int i=0;i<1000;i++)
+        {
+            ChangeDir();
+
+            if (direction == Direction.up || direction == Direction.down)
+            {
+                if ((y + 2*(int)direction > -1 && y + 2*(int)direction < height) && (Maze[x,y+2*(int)direction] != 1))
+                {
+                    y+=(int)direction;
+                    Maze[x,y] = 1;
+                    y+=(int)direction;
+                    Maze[x,y] = 1;
+                }
+                else
+                {
+                    ChangePosition();
+                }
+            }
+            else if (direction == Direction.left || direction == Direction.right)
+            {
+                if ((x + (int)direction > -1 && x + (int)direction < height) && (Maze[x+(int)direction,y] != 1))
+                {
+                    x+=(int)direction/2;
+                    Maze[x,y] = 1;
+                    x+=(int)direction/2;
+                    Maze[x,y] = 1;
+                }
+                else
+                {
+                    ChangePosition();
+                }
+            }
+        }
+    }
+}
