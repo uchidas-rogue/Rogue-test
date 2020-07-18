1753eea6c1fda36d246a44d455ab5f67d4611171
uchidas <sho417sho@gmail.com>
2020-07-18 20:43:36 +0900

    update makemaze room

--- Assets/Script/DungeonSceneA/MakeMaze.cs
+++ Assets/Script/DungeonSceneA/MakeMaze.cs
@@ -11,7 +11,11 @@ public class MakeMaze
     private int x = 1;
     private int y = 1;
     private int roomSize;
+    private int roomWidth;
+    private int roomHeiht;
     private int roomEntry;
+    private int roomEntryX;
+    private int roomEntryY;
     private int roomNumber;
     private List<int[]> FloorPosList = new List<int[]>();
     public MakeMaze(int width, int height)
@@ -30,7 +34,9 @@ public class MakeMaze
     // check the position is in maze 
     private bool CheckInside0Position(int x, int y)
     {
-        return ( x > -1 && x < width) && ( y > -1 && y < width) && (Maze[x,y] == 0);
+        return ( x > -1 && x < this.width)
+                && ( y > -1 && y < this.width)
+                && (this.Maze[x,y] == 0);
     }
     
     // check the position where can dig 
@@ -53,48 +59,48 @@ public class MakeMaze
         }
     }
 
-    private bool CheckCanMakeRoom(int x, int y, int size, int entry)
+    private bool CheckCanMakeRoom(int x, int y, int sizeX, int sizeY, int entryX, int entryY)
     {
-        return (CheckInside0Position(x-entry,y)
-                && CheckInside0Position(x-entry+size-1,y)
-                && CheckInside0Position(x-entry,y+(size-1)*(int)Direction.up)
-                && CheckInside0Position(x-entry+size-1,y+(size-1)*(int)Direction.up)
+        return (CheckInside0Position(x-entryX,y)
+                && CheckInside0Position(x-entryX+sizeX-1,y)
+                && CheckInside0Position(x-entryX,y+(sizeY-1)*(int)Direction.up)
+                && CheckInside0Position(x-entryX+sizeX-1,y+(sizeY-1)*(int)Direction.up)
                 ) 
                 ||
-                (CheckInside0Position(x-entry,y)
-                && CheckInside0Position(x-entry+size-1,y)
-                && CheckInside0Position(x-entry,y+(size-1)*(int)Direction.down)
-                && CheckInside0Position(x-entry+size-1,y+(size-1)*(int)Direction.down)
+                (CheckInside0Position(x-entryX,y)
+                && CheckInside0Position(x-entryX+sizeX-1,y)
+                && CheckInside0Position(x-entryX,y+(sizeY-1)*(int)Direction.down)
+                && CheckInside0Position(x-entryX+sizeX-1,y+(sizeY-1)*(int)Direction.down)
                 ) 
                 ||
-                (CheckInside0Position(x,y-entry)
-                && CheckInside0Position(x,y-entry+size-1)
-                && CheckInside0Position(x+(size-1)*(int)Direction.left/2,y-entry)
-                && CheckInside0Position(x+(size-1)*(int)Direction.left/2,y-entry+size-1)
+                (CheckInside0Position(x,y-entryY)
+                && CheckInside0Position(x,y-entryY+sizeY-1)
+                && CheckInside0Position(x+(sizeX-1)*(int)Direction.left/2,y-entryY)
+                && CheckInside0Position(x+(sizeX-1)*(int)Direction.left/2,y-entryY+sizeY-1)
                 )
                 ||
-                (CheckInside0Position(x,y-entry)
-                && CheckInside0Position(x,y-entry+size-1)
-                && CheckInside0Position(x+(size-1)*(int)Direction.right/2,y-entry)
-                && CheckInside0Position(x+(size-1)*(int)Direction.right/2,y-entry+size-1)
+                (CheckInside0Position(x,y-entryY)
+                && CheckInside0Position(x,y-entryY+sizeY-1)
+                && CheckInside0Position(x+(sizeX-1)*(int)Direction.right/2,y-entryY)
+                && CheckInside0Position(x+(sizeX-1)*(int)Direction.right/2,y-entryY+sizeY-1)
                 );
     }
 
-    private bool CheckCanMakeRoom(int x, int y, int size, int entry, Direction direction)
+    private bool CheckCanMakeRoom(int x, int y, int sizeX, int sizeY, int entryX, int entryY, Direction direction)
     {
         if (direction == Direction.up || direction == Direction.down)
         {
-            return (CheckInside0Position(x-entry,y)
-                    && CheckInside0Position(x-entry+size-1,y)
-                    && CheckInside0Position(x-entry,y+(size-1)*(int)direction)
-                    && CheckInside0Position(x-entry+size-1,y+(size-1)*(int)direction)
+            return (CheckInside0Position(x-entryX,y)
+                    && CheckInside0Position(x-entryX+sizeX-1,y)
+                    && CheckInside0Position(x-entryX,y+(sizeY-1)*(int)direction)
+                    && CheckInside0Position(x-entryX+sizeX-1,y+(sizeY-1)*(int)direction)
                     ); 
         }else //(direction == Direction.left || direction == Direction.right)
         {
-            return (CheckInside0Position(x,y-entry)
-                    && CheckInside0Position(x,y-entry+size-1)
-                    && CheckInside0Position(x+(size-1)*(int)direction/2,y-entry)
-                    && CheckInside0Position(x+(size-1)*(int)direction/2,y-entry+size-1)
+            return (CheckInside0Position(x,y-entryY)
+                    && CheckInside0Position(x,y-entryY+sizeY-1)
+                    && CheckInside0Position(x+(sizeX-1)*(int)direction/2,y-entryY)
+                    && CheckInside0Position(x+(sizeX-1)*(int)direction/2,y-entryY+sizeY-1)
                     );
         }
     }
@@ -102,9 +108,9 @@ public class MakeMaze
     private void CheckFloorPosition()
     {
         FloorPosList.Clear();
-        for (int i = 0; i < (width-1)/2; i++)
+        for (int i = 0; i < (this.width-1)/2; i++)
         {
-            for (int j = 0; j < (height-1)/2; j++)
+            for (int j = 0; j < (this.height-1)/2; j++)
             {
                 if (Maze[2*i+1,2*j+1] != 0)
                 {
@@ -116,9 +122,9 @@ public class MakeMaze
 
     private bool CheckAnyDigPosition()
     {
-        for (int i = 0; i < (width-1)/2; i++)
+        for (int i = 0; i < (this.width-1)/2; i++)
         {
-            for (int j = 0; j < (height-1)/2; j++)
+            for (int j = 0; j < (this.height-1)/2; j++)
             {
                 if (CheckCanDig(2*i+1, 2*j+1))
                 {
@@ -134,16 +140,16 @@ public class MakeMaze
         switch (Random.Range(0,4))
         {
             case 0:
-                direction = Direction.up;
+                this.direction = Direction.up;
                 break;
             case 1:
-                direction = Direction.down;
+                this.direction = Direction.down;
                 break;
             case 2:
-                direction = Direction.left;
+                this.direction = Direction.left;
                 break;
             case 3:
-                direction = Direction.right;
+                this.direction = Direction.right;
                 break;
         }
     }
@@ -156,17 +162,45 @@ public class MakeMaze
         this.y = FloorPosList[randomListNum][1];
     }
 
+    private void ChangeroomSize()
+    {
+        this.roomWidth = Random.Range(4,8);
+        if (this.roomWidth % 2 == 0)
+        {//odd num
+            this.roomWidth++;
+        }
+        this.roomHeiht = Random.Range(4,8);
+        if (this.roomHeiht % 2 == 0)
+        {//odd num
+            this.roomHeiht++;
+        }
+    }
+
+    private void ChangeroomEntry()
+    {
+        this.roomEntryX = Random.Range(0,this.roomWidth+1);
+        if (this.roomEntryX % 2 == 1)
+        {//even num
+            this.roomEntryX--;
+        }
+        this.roomEntryY = Random.Range(0,this.roomHeiht+1);
+        if (this.roomEntryY % 2 == 1)
+        {//even num
+            this.roomEntryY--;
+        }
+    }
+
     private void ChangeroomSizeAndroomEntry()
     {
-        roomSize = Random.Range(4,8);
-        if (roomSize % 2 == 0)
+        this.roomSize = Random.Range(4,8);
+        if (this.roomSize % 2 == 0)
         {//odd num
-            roomSize++;
+            this.roomSize++;
         }
-        roomEntry = Random.Range(0,roomSize+1);
-        if (roomEntry % 2 == 1)
+        this.roomEntry = Random.Range(0,roomSize+1);
+        if (this.roomEntry % 2 == 1)
         {//even num
-            roomEntry--;
+            this.roomEntry--;
         }
     }
 
@@ -174,26 +208,26 @@ public class MakeMaze
     {
         if (direction == Direction.up || direction == Direction.down)
         {
-            for (int i = 0; i < roomSize; i++)
+            for (int i = 0; i < roomWidth; i++)
             {
-                for (int j = 0; j < roomSize; j++)
+                for (int j = 0; j < roomHeiht; j++)
                 {
                     //(Maze[x-entry,y]),(Maze[x-entry,y+(size-1)])
                     //(Maze[x-entry+size-1,y]),(Maze[x-entry+size-1,y+(size-1)])
-                    Maze[this.x-roomEntry+i,this.y+j*(int)direction] = 2;
+                    Maze[this.x-roomEntryX+i,this.y+j*(int)direction] = 2;
                 }
             }
             roomNumber++;
         }
         if (direction == Direction.left || direction == Direction.right)
         {
-            for (int i = 0; i < roomSize; i++)
+            for (int i = 0; i < roomWidth; i++)
             {
-                for (int j = 0; j < roomSize; j++)
+                for (int j = 0; j < roomHeiht; j++)
                 {
                     //(Maze[x,y-entry]),(Maze[x+(size-1),y-entry])
                     //(Maze[x,y-entry+size-1]),(Maze[x+(size-1),y-entry+size-1])
-                    Maze[this.x+i*(int)direction/2,this.y-roomEntry+j] = 2;
+                    Maze[this.x+i*(int)direction/2,this.y-roomEntryY+j] = 2;
                 }
             }
             roomNumber++;
@@ -220,16 +254,16 @@ public class MakeMaze
 
     public void DigMaze()
     {
-        Maze = new int[width,height];
+        Maze = new int[this.width,this.height];
         //entry position x,y oddnum,oddnum
         //x=(width-1)/2 + 1;
         //y=(height-1)/2 + 1;
 
-        ChangeroomSizeAndroomEntry();
-        roomEntry = 0;
-        if (CheckCanMakeRoom(this.x,this.y,this.roomSize,this.roomEntry))
+        ChangeroomSize();
+        this.roomEntry = 0;
+        if (CheckCanMakeRoom(this.x,this.y,this.roomWidth,this.roomHeiht,this.roomEntryX,this.roomEntryY))
         {
-            while (!CheckCanMakeRoom(this.x,this.y,this.roomSize,this.roomEntry,this.direction))
+            while (!CheckCanMakeRoom(this.x,this.y,this.roomWidth,this.roomHeiht,this.roomEntryX,this.roomEntryY,this.direction))
             {
                 ChangeDir();
             }
@@ -238,14 +272,15 @@ public class MakeMaze
         
         int cnt = 0;
 
-        while (cnt < 250)
+        while (cnt < 500)
         {
             ChangeDir();
-            ChangeroomSizeAndroomEntry();
+            ChangeroomSize();
+            ChangeroomEntry();
             
-            if (CheckCanMakeRoom(this.x,this.y,this.roomSize,this.roomEntry))
+            if (CheckCanMakeRoom(this.x,this.y,this.roomWidth,this.roomHeiht,this.roomEntryX,this.roomEntryY))
             {
-                while (!CheckCanMakeRoom(this.x,this.y,this.roomSize,this.roomEntry,this.direction))
+                while (!CheckCanMakeRoom(this.x,this.y,this.roomWidth,this.roomHeiht,this.roomEntryX,this.roomEntryY,this.direction))
                 {
                     ChangeDir();
                 }
