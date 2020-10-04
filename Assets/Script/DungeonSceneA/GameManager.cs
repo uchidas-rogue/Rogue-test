7fe684889bb5d942e0aaa9d0c0f84b29e509d687
uchidas <sho417sho@gmail.com>
2020-10-04 19:48:51 +0900

    オブジェクト取得を一括で行うように変更

--- Assets/Script/DungeonSceneA/GameManager.cs
+++ Assets/Script/DungeonSceneA/GameManager.cs
@@ -10,17 +10,28 @@ using UnityEngine.UI;
 /// </summary>
 public class GameManager : MonoBehaviour
 {
+    // GameManagerのシングルトン
     public static GameManager Singleton;
-    [HideInInspector]
-    private GameObject MessageWindowObject;
-    [HideInInspector]
-    private GameObject PlayerObject;
-    [HideInInspector]
-    private GameObject MiniMapObject;
-    private BoardManager boardScript;
 
+    // Public GameObjects
+    #region Objects
+    [HideInInspector]
+    public GameObject MessageWindowObject;
+
+    [HideInInspector]
+    public GameObject PlayerObject;
+
+    [HideInInspector]
+    public GameObject MiniMapObject;
+
+    [HideInInspector]
+    public GameObject FloorNumTextObject;
+
+    #endregion
+
+    // Public Parameters
+    #region Params
     private bool _playersTurn = true; //trueならプレイヤー移動可能
-
     [HideInInspector]
     public bool playersTurn
     {
@@ -32,12 +43,15 @@ public class GameManager : MonoBehaviour
         {
             if (_playersTurn != value)
             {
-                SetMiniMapString ();
+                MiniMapObject.GetComponent<MinimapControll> ().SetMiniMapString ();
             }
             _playersTurn = value;
         }
     }
-    private TextMeshProUGUI floorNumText;
+
+    #endregion
+
+    private BoardManager boardScript;
     private int floorNum = 0;
     private string mapString;
 
@@ -52,145 +66,61 @@ public class GameManager : MonoBehaviour
         {
             Destroy (gameObject);
         }
-        //the gameobject(GameManager is attached to) dont destroy while game awaking
+        // the gameobject(GameManager is attached to) dont destroy while game awaking
         DontDestroyOnLoad (gameObject);
 
-        //BoardManager
+        // BoardManager
         boardScript = GetComponent<BoardManager> ();
 
-        //sceneLoadedにonsceneloadeを追記
+        // sceneLoadedにonsceneloadeを追記
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
+    /// <summary>
+    ///  シーン開始時に必ず行う処理
+    /// </summary>
     void InitGame ()
     {
-        GetObjectinInitGame ();
+        GetObjects ();
 
-        //make map
-        boardScript.SetupScene ();
-        //make minimap
-        Singleton.SetMiniMapString ();
-
-        //disable messagewindow
+        // disable messagewindow
         this.MessageWindowObject.SetActive (false);
 
-        this.floorNum++;
-        this.floorNumText = GameObject.Find ("FloorNumText").GetComponent<TextMeshProUGUI> ();
-        this.floorNumText.text = floorNum + "F";
+        // make map
+        boardScript.SetupScene ();
+        // make minimap
+        MiniMapObject.GetComponent<MinimapControll> ().SetMiniMapString ();
 
-        Singleton.SetStringToMessageWindow ("ゲームが開始されました\nここから冒険は始まる！！");
-        Singleton.SetStringToMessageWindow ("二回目の呼び出しです\nここから冒険は再び始まる！！");
+        SetStringToFloorNumText();
+
+        SetStringToMessageWindow ("ゲームが開始されました\nここから冒険は始まる！！");
+        SetStringToMessageWindow ("二回目の呼び出しです\nここから冒険は再び始まる！！");
     }
 
-    private void GetObjectinInitGame ()
+    /// <summary>
+    ///  シーンのオブジェクトを取得する
+    /// </summary>
+    private void GetObjects ()
     {
         PlayerObject = GameObject.Find ("Player").gameObject;
         MessageWindowObject = GameObject.Find ("MessageWindow").gameObject;
         MiniMapObject = GameObject.Find ("MiniMap").gameObject;
+        FloorNumTextObject = GameObject.Find ("FloorNumText").gameObject;
     }
 
     private void SetStringToMessageWindow (string msg)
     {
-        MessageWindowObject.SetActive (true);
-        MessageWindowObject.GetComponent<MessageSend> ().message = msg;
+        MessageWindowObject.GetComponent<MessageWindowControll> ().message = msg;
     }
 
-    private void CheckFloor (int j, int i)
+    private void SetStringToFloorNumText()
     {
-        if (boardScript.Maze[j, i, 1] == 0 && boardScript.Maze[j, i, 0] == 2)
-        {
-            boardScript.Maze[j, i, 1] = 1;
-            TurnToWalked (j, i);
-        }
-        else
-        {
-            boardScript.Maze[j, i, 1] = 1;
-        }
-    }
-
-    private void TurnToWalked (int j, int i)
-    {
-        if (boardScript.Maze[j, i, 0] == 2)
-        {
-            CheckFloor (j - 1, i - 1);
-            CheckFloor (j - 1, i);
-            CheckFloor (j - 1, i + 1);
-            CheckFloor (j, i - 1);
-            CheckFloor (j, i + 1);
-            CheckFloor (j + 1, i - 1);
-            CheckFloor (j + 1, i);
-            CheckFloor (j + 1, i + 1);
-        }
-        else
-        {
-            boardScript.Maze[j - 1, i - 1, 1] = 1;
-            boardScript.Maze[j - 1, i, 1] = 1;
-            boardScript.Maze[j - 1, i + 1, 1] = 1;
-            boardScript.Maze[j, i - 1, 1] = 1;
-            boardScript.Maze[j, i + 1, 1] = 1;
-            boardScript.Maze[j + 1, i - 1, 1] = 1;
-            boardScript.Maze[j + 1, i, 1] = 1;
-            boardScript.Maze[j + 1, i + 1, 1] = 1;
-        }
-    }
-
-    private void SetMiniMapString ()
-    {
-        mapString = "";
-
-        TurnToWalked ((int) PlayerObject.transform.position.x, (int) PlayerObject.transform.position.y);
-
-        for (int i = boardScript.width - 1; i >= 0; i--)
-        {
-            for (int j = 0; j < boardScript.height; j++)
-            {
-                if (j == PlayerObject.transform.position.x && i == PlayerObject.transform.position.y)
-                { //player position
-                    mapString += "<color=yellow>●</color>";
-                }
-                else if (boardScript.Maze[j, i, 0] == 3)
-                { //exit position
-                    if (boardScript.Maze[j, i, 1] == 1)
-                    {
-                        mapString += "<color=green>■</color>";
-                    }
-                    else
-                    {
-                        mapString += "   ";
-                    }
-                }
-                else if (boardScript.Maze[j, i, 0] == 1 || boardScript.Maze[j, i, 0] == 2)
-                { //floor position
-                    if (boardScript.Maze[j, i, 1] == 1)
-                    {
-                        mapString += "<color=blue>■</color>";
-                    }
-                    else
-                    {
-                        mapString += "   ";
-                    }
-                }
-                else
-                { //wall position
-                    if (boardScript.Maze[j, i, 1] == 1)
-                    {
-                        mapString += "■";
-                    }
-                    else
-                    {
-                        mapString += "   ";
-                    }
-                }
-            }
-            mapString += "\n";
-        }
-        MiniMapObject.GetComponent<MinimapControll> ().SetmapText (mapString);
+        this.floorNum++;
+        FloorNumTextObject.GetComponent<TextMeshProUGUI> ().text = floorNum + "F";
     }
 
     static private void OnSceneLoaded (Scene arg0, LoadSceneMode arg1)
     {
-        //get messagewindow
-        //Singleton.MessageWindowObject = GameObject.Find ("MessageWindowObject");
         Singleton.InitGame ();
     }
 
